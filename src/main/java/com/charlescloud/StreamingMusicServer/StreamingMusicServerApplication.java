1ffce8f224e8af1a458add465b1c0ac52032e16a
Charles Cloud <charliecloud@gmail.com>
2020-11-09 07:22:53 -0500

    Add boilerplate initalization code

---
+++ src/main/java/com/charlescloud/StreamingMusicServer/StreamingMusicServerApplication.java
@@ -0,0 +1,13 @@
+package com.charlescloud.StreamingMusicServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamingMusicServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamingMusicServerApplication.class, args);
+	}
+
+}
